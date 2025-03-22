68fce275f41a80baa5f9c8f5e60df3c89fa28e5c
Gerhard Radatz <gerhard.radatz@gmail.com>
2025-03-22 12:31:52 +0100

    Initial commit

---
+++ src/main/java/com/grit/intellij/jdoclinkhelper/AskQuestionAction.java
@@ -0,0 +1,14 @@
+package com.grit.intellij.jdoclinkhelper;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class AskQuestionAction extends AnAction {
+
+	@Override
+	public void actionPerformed(@NotNull AnActionEvent e) {
+		BrowserUtil.browse("https://stackoverflow.com/questions/ask");
+	}
+}
